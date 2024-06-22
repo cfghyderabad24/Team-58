51b94265abb6f04e2ea710a0b3e3e828c772ca9f
devnouiq <dev@nouiq.com>
2024-06-23 02:30:40 +0530

    Updated donor pages

--- src/models/donors.py
+++ src/models/donors.py
@@ -2,4 +2,130 @@ from database import donors_collection
 from bson import ObjectId
 from typing import List
 
+donors_projection = {
+    "_id": {'$toString': "$_id"},
+    "S.No": 1,
+    "Company": 1,
+    "CSR Project(s)": 1,
+    "Development Sector(s)": 1,
+    "State": 1,
+    "District": 1,
+    "Projected Amount Outlay(INR Cr.)": 1,
+    "Amount Spent(INR Cr.)": 1,
+    "Mode of Implementation": 1,
+    "Year": 1
+  }
+
+def get_donors(query: None, sector: None, state: None, district: None, mode_of_implentation : None, page):
+    page_size=9
+    must_clauses = []
+    filter_clauses = []
+    should_clauses = []
+
+
+    if sector:
+        must_clauses.append({
+            'text': {
+                'query': sector,
+                'path': 'Development Sector(s)'
+            }
+        })
+    if state:
+        must_clauses.append({
+            'text': {
+                'query': state,
+                'path': 'State'
+            }
+        })
+    if district:
+         must_clauses.append({
+            'text': {
+                'query': district,
+                'path': 'District'
+            }
+        })
+    if mode_of_implentation:
+         must_clauses.append({
+            'text': {
+                'query': mode_of_implentation,
+                'path': 'Mode of Implementation'
+            }
+        })
+        
+
+
+    if query:
+        should_clauses.append({
+
+            'text': {
+                'query': query,
+                'path': ['Company', 'CSR Project(s)', 'Development Sector(s)', 'State', 'District', 'Mode of Implementation']
+            }
+        })
+
+    minimumShouldMatch = 1 if len(should_clauses) > 0 else 0 
+
+    aggregation_pipeline = [
+        
+        {
+            '$search': {
+                'index': "jaldhaarsearchindex",
+                'compound': {
+                    'filter': filter_clauses,
+                    'must': must_clauses,
+                    'should' : should_clauses,
+                    'minimumShouldMatch':  minimumShouldMatch
+                    
+                }
+            }
+        },
+        {'$project': donors_projection },
+    ]
+
+
+    aggregation_pipeline = [stage for stage in aggregation_pipeline if stage]
+
+    events = list(donors_collection.aggregate(aggregation_pipeline))
+
+    total_matchable_count = len(events)
+    total_pages = total_matchable_count // page_size + (1 if total_matchable_count % page_size > 0 else 0)
+
+    paginated_events = {i: [] for i in range(1, total_pages + 1)}
+    for i in range(total_matchable_count):
+        page_num = (i // page_size) + 1
+        paginated_events[page_num].append(events[i])
+
+
+    return {"events": paginated_events.get(page, []), "total_pages": total_pages}
+
+
+def get_filter_list(filter_name):
+    if filter_name == "Development Sector(s)" or filter_name == "State" or filter_name == "District" or filter_name == "Mode of Implementation":
+        pipeline = [
+            {
+                '$group': {
+                    '_id': f'${filter_name}',
+                    'count': {'$sum': 1}
+                }
+            },
+            {
+                '$sort': {'count': -1}
+            },
+            {
+                '$limit': 5
+            },
+            {
+                '$project': {
+                    '_id': 0,
+                    f'{filter_name}': '$_id',
+                    'count': 1
+                }
+            }
+        ]
+
+        result = [filter[filter_name] for filter in list(donors_collection.aggregate(pipeline))]
+        return result
+    else:
+        return False
+    
 
--- src/routes/donors.py
+++ src/routes/donors.py
@@ -1,10 +1,23 @@
-from fastapi import APIRouter, HTTPException
-
+from fastapi import APIRouter, HTTPException, Query
+from src.models.donors import get_donors, get_filter_list
+from typing import List
+from starlette import status
+from src.routes.auth import user_dependency
 
 router  = APIRouter()
 
-@router.get("/v1/donors")
-def get_donors():
-    return {"donors": "donors"}
+@router.get("/v1/fetch-donors")
+def fetch_donors(user: user_dependency, query: str= None, sector: List[str] = Query(None), state: List[str] = Query(None), district: List[str] = Query(None), mode_of_implentation : List[str] = Query(None), page_number : int = 1):
+    results = get_donors(query, sector, state, district, mode_of_implentation, page_number)
+    if results:
+        return {"data" : results}
+    else:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="No donors found")
 
-
+@router.get("/v1/fetch-filters")
+def get_filters(user: user_dependency, filter_name: str = None):
+    filters = get_filter_list(filter_name)
+    if filters:
+        return {"details" : {"filters" : filters} }
+    else:
+        return {"details" : {"filters" : []} }
