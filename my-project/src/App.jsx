2fe32c9f9b9f201d331fd99d29dac577885d12a4
Aashirya04 <aachandaka@gmail.com>
2024-06-22 22:48:32 +0530

    frontend initiated

---
+++ my-project/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './App.css'
+import NavBar from "./compnents/NavBar";
+
+function App() {
+ 
+  return (
+   <div>
+    <NavBar />
+   </div>
+  )
+}
+
+export default App
